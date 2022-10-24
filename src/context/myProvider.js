003022ee04c1aa7196a7602d6f5c8704a08d4564
Pedro <pedrosteinmuller10105@hotmail.com>
2022-10-24 15:35:53 -0300

    configuracao inicial do projeto

---
+++ src/context/myProvider.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import MyContext from './myContext';
+
+function Provider({ children }) {
+  const [email, setEmail] = useState('');
+
+  return (
+    <MyContext.Provider value={ email }>
+      {children}
+    </MyContext.Provider>
+  );
+}
+
+Provider.propTypes = { children: PropTypes.node.isRequired };
+
+export default Provider;
