839028d86cdacac9876f0e11e2bd30ea646d54f9
Ebraim Oliveira <ebraim_oliveira@hotmail.com>
2022-10-24 17:57:13 -0300

    componentes de login criados, validados e salvos

--- src/page/Login.js
+++ src/page/Login.js
@@ -22,29 +22,36 @@ function Login() {
     verifyButton();
   }, [email, password]);
 
+  const loginSubmit = (event) => {
+    event.preventDefault();
+    localStorage.setItem('user', JSON.stringify({
+      email,
+    }));
+  };
+
   return (
     <>
       <p>teste_teste@gmail.com</p>
       <div>Login</div>
-      <form>
+      <form onSubmit={ loginSubmit }>
         <input
           data-testid="email-input"
           type="email"
           name="email"
-          value={email}
-          onChange={handleEmail}
+          value={ email }
+          onChange={ handleEmail }
         />
         <input
           data-testid="password-input"
           type="password"
           name="password"
-          value={password}
-          onChange={handlePassword}
+          value={ password }
+          onChange={ handlePassword }
         />
         <button
           data-testid="login-submit-btn"
           type="submit"
-          disabled={loginBtnDisabled}
+          disabled={ loginBtnDisabled }
         >
           Enter
         </button>
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,10 +1,37 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from '../App';
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import Login from '../page/Login';
 
-test('Farewell, front-end', () => {
-  // Este arquivo pode ser modificado ou deletado sem problemas
-  render(<App />);
-  const linkElement = screen.getByText(/TRYBE/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// const passwordText = 'password-input';
+// const emailText = 'email-input';
+
+// describe('Testa tela de Login', () => {
+//   test('Possui um input para email', () => {
+//     render(<Login />);
+//     const emailInput = screen.getByTestId('email-input');
+//     expect(emailInput).toBeInTheDocument();
+//   });
+
+//   test('Possui um input para senha', () => {
+//     render(<Login />);
+//     const passwordInput = screen.getByTestId('password-input');
+//     expect(passwordInput).toBeInTheDocument();
+//   });
+
+//   test('Possui um botão', () => {
+//     render(<Login />);
+//     const button = screen.getByText('Enter');
+//     expect(button).toBeInTheDocument();
+//   });
+
+//   test('Se o botao ativa ao preencher os inputs corretamente', () => {
+//     render(<Login />);
+//     const emailInput = screen.getByTestId(emailText);
+//     const passwordInput = screen.getByTestId(passwordText);
+//     const button = screen.getByText('Enter');
+//     userEvent.type(emailInput, 'email@provider.com');
+//     userEvent.type(passwordInput, '123456');
+//     expect(button).toBeEnabled();
+//   });
+// });
