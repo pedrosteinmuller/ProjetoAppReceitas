c52a27393f411b6402dbdcfbfac3ba258d19d7e3
carlosbprado <carlosbprado@outlook.com>
2022-10-25 16:05:55 -0300

    Requisitos do footer feitos

--- src/page/Drinks.js
+++ src/page/Drinks.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function Drinks() {
   return (
     <div>
       <Header verifyPage={ false } />
       <h1 data-testid="page-title">Drinks</h1>
+      <Footer />
     </div>
   );
 }
--- src/page/Meals.js
+++ src/page/Meals.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Meals() {
@@ -6,6 +7,7 @@ function Meals() {
     <div>
       <Header verifyPage={ false } />
       <h1 data-testid="page-title">Meals</h1>
+      <Footer />
     </div>
   );
 }
