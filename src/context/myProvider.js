1eb5c186141a7f8e66ac05e12e8fef0450760e3a
Pedro <pedrosteinmuller10105@hotmail.com>
2022-10-25 20:39:27 -0300

    refatorando codigos para faciliar nos proximos requisitos

--- src/context/myProvider.js
+++ src/context/myProvider.js
@@ -1,11 +1,19 @@
 import PropTypes from 'prop-types';
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useCallback } from 'react';
+import { useLocation, useHistory } from 'react-router-dom';
+
 import MyContext from './myContext';
+import { fetchApiIngredients, fetchFirstLetter, fetchName } from '../services/fetchApi';
 
 function Provider({ children }) {
   const [email, setEmaill] = useState('');
   const [password, setPassword] = useState('');
   const [search, setSearch] = useState('');
+  const [data, setData] = useState([]);
+  const [radio, setRadio] = useState('');
+
+  const { pathname } = useLocation();
+  const history = useHistory();
 
   const handleEmail = ({ target }) => {
     setEmaill(target.value);
@@ -17,15 +25,53 @@ function Provider({ children }) {
 
   const handleSearch = ({ target: { value } }) => setSearch(value);
 
+  const handleChangeRadio = ({ target: { value } }) => { setRadio(value); };
+
+  const handleFilterResults = useCallback(async () => {
+    const path = pathname.includes('meals') ? 'themealdb' : 'thecocktaildb';
+    const verifyIdProduct = pathname.includes('meals') ? 'idMeal' : 'idDrink';
+    let getProducts = data;
+    switch (radio) {
+    case 'ingredient':
+      getProducts = await fetchApiIngredients(search, path);
+      setData(getProducts);
+
+      setSearch('');
+      break;
+    case 'name':
+      getProducts = await fetchName(search, path);
+      setData(getProducts);
+
+      setSearch('');
+      break;
+    default:
+      if (search.length === 1) {
+        getProducts = await fetchFirstLetter(search, path);
+        setData(getProducts);
+
+        setSearch('');
+      } else {
+        global.alert('Your search must have only 1 (one) character');
+      }
+      break;
+    }
+    console.log(data);
+    if (getProducts.length === 1) {
+      history.push(`${pathname}/${getProducts[0][verifyIdProduct]}`);
+    }
+  }, [data, history, pathname, radio, search]);
+
   const context = useMemo(() => ({
     handleEmail,
     handleSearch,
+    handleFilterResults,
+    handleChangeRadio,
     search,
     email,
     setSearch,
     handlePassword,
     password,
-  }), [email, password, search]);
+  }), [email, handleFilterResults, password, search]);
 
   return (
     <MyContext.Provider value={ context }>
