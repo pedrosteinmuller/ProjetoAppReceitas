bbb381ecf2b831e8c6e5140e2a07e12177909992
Pedro <pedrosteinmuller10105@hotmail.com>
2022-10-26 09:52:53 -0300

    resolucao requisito14

--- src/page/Drinks.js
+++ src/page/Drinks.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import myContext from '../context/myContext';
 
 function Drinks() {
+  const { data } = useContext(myContext);
+  const MAX_LENGTH = 12;
   return (
     <div>
       <Header verifyPage={ false } />
       <h1 data-testid="page-title">Drinks</h1>
+      {
+        data?.map((drink, index) => index < MAX_LENGTH && (
+          <div data-testid={ `${index}-recipe-card` } key={ drink.idDrink }>
+            <img
+              data-testid={ `${index}-card-img` }
+              src={ drink.strDrinkThumb }
+              alt={ drink.strDrink }
+            />
+            <h1 data-testid={ `${index}-card-name` }>{ drink.strDrink }</h1>
+          </div>
+        ))
+      }
       <Footer />
     </div>
   );
