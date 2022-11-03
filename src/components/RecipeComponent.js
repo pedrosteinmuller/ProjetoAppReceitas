8281cb515c655334aa18fe6fc48a115689b15c2b
Pedro <pedrosteinmuller10105@hotmail.com>
2022-11-03 18:24:54 -0300

    resolucao requisito 40

--- src/components/RecipeComponent.js
+++ src/components/RecipeComponent.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { useLocation, useParams } from 'react-router-dom';
 import copy from 'clipboard-copy';
@@ -11,42 +11,34 @@ function RecipeComponent({ recipeInProgress }) {
   const param = useParams();
   const [favoriteButton, setFavoriteButton] = useState(false);
   const [click, setClick] = useState(false);
-  // const [checked, setChecked] = useState(true);
-
-  const ingredientList = Object.entries(recipeInProgress)
-    .filter((item) => item[0].includes('strIngredient') && item[1] !== '')
-    .filter((item) => item[1] !== null)
-    .filter((item) => item[1] !== undefined)
-    .map((ingredient) => ingredient[1]);
-  const measureList = Object.entries(recipeInProgress)
-    .filter((item) => item[0].includes('strMeasure'))
-    .filter((item) => item[1] !== ' ')
-    .filter((item) => item[1] !== null)
-    .filter((item) => item[1] !== undefined)
-    .map((measure) => measure[1]);
-
-  const markCheckbox = (e) => {
-    const label = document.querySelectorAll('label[class=label]')[e];
-    if (label.style.textDecoration !== '') {
-      label.style.textDecoration = '';
-      return;
-    }
-    label.style.textDecoration = 'line-through solid rgb(0, 0, 0)';
-  };
-
-  useEffect(() => {
-    const favorite = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
-    setFavoriteButton(favorite.some((item) => item.id === param.id));
-  }, []);
+  const path = pathname.includes('meals') ? 'meals' : 'drinks';
+  const [ingredients, setIngredients] = useState([]);
+  const [measure, setMeasure] = useState([]);
+  const [loading, setLoading] = useState(false);
+  // const [checked, setChecked] = useState(false);
+  const handleIngredient = useCallback(() => {
+    const ingredientList = Object.entries(recipeInProgress)
+      .filter((item) => item[0].includes('strIngredient') && item[1] !== '')
+      .filter((item) => item[1] !== null)
+      .filter((item) => item[1] !== undefined)
+      .map((ingredient) => ingredient[1]);
+    const measureList = Object.entries(recipeInProgress)
+      .filter((item) => item[0].includes('strMeasure'))
+      .filter((item) => item[1] !== ' ')
+      .filter((item) => item[1] !== null)
+      .filter((item) => item[1] !== undefined)
+      .map((measuree) => measuree[1]);
+    setIngredients(ingredientList);
+    setMeasure(measureList);
+  }, [recipeInProgress]);
 
   const btnCopy = () => {
-    const path = pathname.includes('meals') ? 'meals' : 'drinks';
     copy(`http://localhost:3000/${path}/${param.id}`);
     setClick(true);
   };
 
   const favoriteRecipe = () => {
-    const path = pathname.includes('meals') ? 'meal' : 'drink';
+    const pathType = pathname.includes('meals') ? 'meal' : 'drink';
     const productName = pathname
       .includes('meals') ? recipeInProgress.strMeal : recipeInProgress.strDrink;
     const productImage = pathname
@@ -55,7 +47,7 @@ function RecipeComponent({ recipeInProgress }) {
       .includes('meals') ? recipeInProgress.strArea : '';
     const objFavorite = {
       id: param.id,
-      type: path,
+      type: pathType,
       nationality: productNationality,
       category: recipeInProgress.strCategory,
       alcoholicOrNot: recipeInProgress.strAlcoholic || '',
@@ -72,11 +64,38 @@ function RecipeComponent({ recipeInProgress }) {
     }
   };
 
-  const getLsInfo = lsInProgressRecipes();
+  useEffect(() => {
+    const favorite = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
+    setFavoriteButton(favorite.some((item) => item.id === param.id));
+    const getLsInfo = lsInProgressRecipes();
 
-  if (!Object.keys(getLsInfo.meals).includes(param.id)) {
-    getLsInfo.meals[param.id] = [];
-  }
+    if (!getLsInfo[path][param.id] && ingredients.length > 0) {
+      getLsInfo[path][param.id] = [];
+    }
+    localStorage.setItem('inProgressRecipes', JSON.stringify(getLsInfo));
+  }, [ingredients, param.id, path]);
+
+  useEffect(() => {
+    handleIngredient();
+  }, [handleIngredient, recipeInProgress]);
+
+  const markCheckbox = (ingredient, index) => {
+    const getLsInfo = lsInProgressRecipes();
+    const label = document.querySelectorAll('label[class=label]')[index];
+    if (getLsInfo[path][param.id].includes(ingredient)
+    && label.style.textDecoration !== '') {
+      label.style.textDecoration = '';
+      const getLsInfoUpdate = getLsInfo[path][param.id]
+        .filter((item) => item !== ingredient);
+      getLsInfo[path][param.id] = [...getLsInfoUpdate];
+      localStorage.setItem('inProgressRecipes', JSON.stringify(getLsInfo));
+    } else {
+      label.style.textDecoration = 'line-through solid rgb(0, 0, 0)';
+      getLsInfo[path][param.id] = [...getLsInfo[path][param.id], ingredient];
+      localStorage.setItem('inProgressRecipes', JSON.stringify(getLsInfo));
+    }
+    setLoading(!loading);
+  };
 
   return (
     <>
@@ -133,20 +152,22 @@ function RecipeComponent({ recipeInProgress }) {
         {recipeInProgress.strInstructions}
       </p>
       {
-        ingredientList?.map(
+        ingredients?.map(
           (ingredient, index) => (
             <div key={ index }>
               <label
-                htmlFor={ ingredient }
+                htmlFor={ `${ingredient}${index}` }
                 data-testid={ `${index}-ingredient-step` }
                 className="label"
               >
-                {`${measureList[index] || ''} ${ingredient}`}
+                {`${measure[index] || ''} ${ingredient}`}
                 <input
                   type="checkbox"
+                  id={ `${ingredient}${index}` }
                   value={ ingredient }
-                  // disabled={  }
-                  onChange={ () => markCheckbox(index) }
+                  checked={ lsInProgressRecipes()[path][param.id]
+                    ? lsInProgressRecipes()[path][param.id].includes(ingredient) : false }
+                  onChange={ () => markCheckbox(ingredient, index) }
                 />
               </label>
             </div>
--- src/services/recipesLocalStorage.js
+++ src/services/recipesLocalStorage.js
@@ -1,14 +1,12 @@
 const lsInProgressRecipes = () => {
-  const getLsInfo = JSON.parse(localStorage.getItem('inProgressRecipes'));
+  // const getLsInfo = JSON.parse(localStorage.getItem('inProgressRecipes'));
 
-  if (getLsInfo === null) {
-    localStorage.setItem('inProgressRecipes', JSON.stringify({ drinks: {}, meals: {} }));
-  }
-
-  console.log(getLsInfo);
+  // if (getLsInfo === null) {
+  // localStorage.setItem('inProgressRecipes', JSON.stringify({ drinks: {}, meals: {} }));
+  // }
 
   const getLsIn = JSON.parse(localStorage.getItem('inProgressRecipes'));
-  return getLsIn;
+  return getLsIn || { drinks: {}, meals: {} };
 };
 
 export default lsInProgressRecipes;
